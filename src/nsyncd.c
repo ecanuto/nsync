49be6179ac671d8d57e34c8f18679af699558c90
Everaldo Canuto <everaldo.canuto@gmail.com>
2013-10-01 12:15:25 -0300

    Signal handler implemented

--- src/nsyncd.c
+++ src/nsyncd.c
@@ -17,16 +17,67 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
 #include "logger.h"
+
+bool terminate;
+
+void signal_handler(int signal)
+{
+	switch (signal)
+	{
+		case SIGHUP:
+			logger(LOG_WARNING, "Received SIGHUP signal");
+			terminate = true;
+			break;
+
+		case SIGINT:
+			logger(LOG_WARNING, "Received SIGINT signal");
+			terminate = true;
+			break;
+
+		case SIGTERM:
+			logger(LOG_WARNING, "Received SIGTERM signal");
+			terminate = true;
+			break;
+
+		case SIGQUIT:
+			logger(LOG_WARNING, "Received SIGQUIT signal");
+			terminate = true;
+			break;
+
+		default:
+			logger(LOG_WARNING, "Unhandled signal %s", strsignal(signal));
+			terminate = true;
+			break;
+	}
+}
 
 int main(int argc, char *argv[])
 {
+	terminate = false;
+
 	// initialize logging
 	logger_open("nsyncd.log");
+
+	// signal handler
+	signal(SIGHUP,  signal_handler);
+	signal(SIGINT,  signal_handler);
+	signal(SIGTERM, signal_handler);
+	signal(SIGQUIT, signal_handler);
 
 	logger(LOG_NOTICE, "Starting %s %s", PACKAGE_NAME, PACKAGE_VERSION);
 
 	printf("%s %s\n", PACKAGE_NAME, PACKAGE_VERSION);
+
+	// main looping
+	while (!terminate)
+	{
+		logger(LOG_DEBUG, "beat");
+		sleep(1);
+	}
 
 	// close logging
 	logger_close();
